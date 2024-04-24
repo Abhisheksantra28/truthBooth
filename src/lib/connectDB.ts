ccb50e0cb1856110e807eeee2112a0233f70db76
Abhishek Santra <abhisheksantra28@gmail.com>
2024-04-24 19:32:46 +0530

    Add .env and .env.sample files, and connectDB.ts module

---
+++ src/lib/connectDB.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function connectDB(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Database is already connected");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI! || "");
+    console.log("db: ", db);
+
+    connection.isConnected = db.connections[0].readyState;
+    
+    console.log("Database is connected successfully!");
+  } catch (error) {
+    console.log("Error connecting to database: ", error);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
